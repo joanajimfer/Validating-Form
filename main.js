e59c4633ed01c06d147a7cf36af4c17b878f9c1f
joanajimfer <joanajimfer@gmail.com>
2019-09-09 10:23:56 +0200

    Correct format and ticks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function validateForm() {
     const userName = document.querySelector("[name=name]");
@@ -29,66 +29,66 @@ function validateForm() {
     //Function to validate user name input
     if((userName.value) === "" || (userName.value.length) >= 15) {
         userName.style.backgroundColor="red";
-        alert('El campo Nombre es obligatorio y debe contener menos de 15 caracteres');
+        alert("El campo Nombre es obligatorio y debe contener menos de 15 caracteres");
         return false;
     }
 
     //Function to validate user lastname input
     else if((userLastname.value) === "" || (userLastname.value.length) >= 30) {
         userLastname.style.backgroundColor="red";
-        alert('El campo Apellidos es obligatorio y debe contener menos de 30 caracteres');
+        alert("El campo Apellidos es obligatorio y debe contener menos de 30 caracteres");
         return false;
     }
 
     //Function to validate user email input
     else if((userEmail.value) === "" || (!emailRegex.test(userEmail.value))){
         userEmail.style.backgroundColor = "red";
-        alert('El campo Email es obligatorio y debe contener un email válido con el formato nombre@example.es');
+        alert("El campo Email es obligatorio y debe contener un email válido con el formato nombre@example.es");
         return false
     }
 
     //Function to validate user city
     else if((userCity.value) === "" || (userCity.value.length) >= 15){
         userCity.style.backgroundColor = "red";
-        alert('El campo Ciudad es obligatorio y debe contener menos de 15 caracteres');
+        alert("El campo Ciudad es obligatorio y debe contener menos de 15 caracteres");
         return false
     }
 
     //Function to validate user post code
     else if((userPostCode.value) === "" || (!postCodeRegex.test(userPostCode.value))){
         userPostCode.style.backgroundColor = "red";
-        alert('El campo Código Postal es obligatorio y debe contener un código postal válido');
+        alert("El campo Código Postal es obligatorio y debe contener un código postal válido");
         return false
     }
 
     //Function to validate user alias name
     else if((userAlias.value) === "" || (!userAliasRegex.test(userAlias.value))){
         userAlias.style.backgroundColor = "red";
-        alert('El campo Nombre de usuario es obligatorio y no puede contener caracteres especiales');
+        alert("El campo Nombre de usuario es obligatorio y no puede contener caracteres especiales");
         return false
     }
 
     //Function to validate user password
     else if((userPassword.value) === "" || (!userPasswordRegex.test(userPassword.value))){
         userPassword.style.backgroundColor = "red";
-        alert('El campo Clave es obligatorio y debe contener al menos 8 caracteres, una mayúscula, número, letra y caracter especial');
+        alert("El campo Clave es obligatorio y debe contener al menos 8 caracteres, una mayúscula, número, letra y caracter especial");
         return false
     }
 
     //Function to validate confirm password
     else if((userConfirmPassword.value) === "" || ((userConfirmPassword.value) !== (userPassword.value))){
         userConfirmPassword.style.backgroundColor = "red";
-        alert('El campo Vuelve a escribir la clave es obligatorio y debe ser igual al campo Clave');
+        alert("El campo Vuelve a escribir la clave es obligatorio y debe ser igual al campo Clave");
         return false
     }
 
     //Function to accept agreement by user checked
     else if(!userAcceptAgreement.checked) {
-        alert('Debe aceptar el acuerdo de servicios, las cookies y la declaración de privacidad para continuar');
+        alert("Debe aceptar el acuerdo de servicios, las cookies y la declaración de privacidad para continuar");
         return false
     }
     else {
-        alert('Gracias por registrarte');
+        alert("Gracias por registrarte");
     }
 }
 
